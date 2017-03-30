933571b49b3caa2f3037d22f0af009a38623294c
jbessert <grwcbessert@gmail.com>
2017-03-29 22:49:35 -0400

    Replace ValuesController with HelloWorldController.

---
+++ src/TerryBerryCore/API/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TerryBerryCore.Domain.Abstract;
+using TerryBerryCore.Infrastructure;
+
+namespace TerryBerryCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloWorldController : Controller
+    {
+        private IHelloWorldRepository repo = new HelloWorldRepository();
+
+        public IActionResult GetMessage()
+        {
+            var result = repo.GetMessages();
+
+            return Json(result);
+        }
+    }
+}
