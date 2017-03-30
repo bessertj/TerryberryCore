773322a4a6a62cee604b128985ad0368b58a65db
jbessert <grwcbessert@gmail.com>
2017-03-29 21:12:07 -0400

    Install and register Autofac.

--- src/TerryBerryCore/Startup.cs
+++ src/TerryBerryCore/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 
 namespace TerryBerryCore
 {
@@ -29,19 +31,35 @@ namespace TerryBerryCore
             Configuration = builder.Build();
         }
 
+        public IContainer ApplicationContainer { get; private set; }
+
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            // Create the container builder.
+            var builder = new ContainerBuilder();
+
+            // Register dependencies, populate the service from
+            // the collection, and build the container. If you want
+            // to dispose of the container at the end of the app, 
+            // be sure to keep a reference to it as a property or field.
+            // builder.Register<MyType>().As<IMyType>();
+            builder.Populate(services);
+            ApplicationContainer = builder.Build();
+
+            // Create the IServiceProvider based on the container.
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -51,6 +69,10 @@ namespace TerryBerryCore
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
+
+            // If you want to dispose of resources that have been resolved in the 
+            // application container, register for the "Application Stopped" event.
+            appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
         }
     }
 }
